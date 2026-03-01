7e50a5305ff1527b6bd66947d88302a024732b6d
Kerem Erkan <kerem.erkan@gmail.com>
2026-03-01 18:49:25 +0300

    Bump version to 0.5.3

--- Sources/asc-client/ASCClient.swift
+++ Sources/asc-client/ASCClient.swift
@@ -4,7 +4,7 @@ import Foundation
 
 @main
 struct ASCClient: AsyncParsableCommand {
-  static let appVersion = "0.5.2"
+  static let appVersion = "0.5.3"
 
   static let configuration = CommandConfiguration(
     commandName: "asc-client",
