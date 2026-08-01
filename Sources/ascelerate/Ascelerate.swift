878e5aeb58ea49135968dc1def17131ebc1708c0
Kerem Erkan <kerem.erkan@gmail.com>
2026-08-01 16:37:17 +0300

    Bump to 0.15.0

--- Sources/ascelerate/Ascelerate.swift
+++ Sources/ascelerate/Ascelerate.swift
@@ -4,7 +4,7 @@ import Foundation
 
 @main
 struct Ascelerate: AsyncParsableCommand {
-  static let appVersion = "0.14.1"
+  static let appVersion = "0.15.0"
 
   static let configuration = CommandConfiguration(
     commandName: "ascelerate",
